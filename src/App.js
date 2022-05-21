f4ee9ffdd7e7786d8b2bff959fa8312da3148367
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-05-21 21:14:12 +0300

    Додав localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Section from "./components/Section";
 import ContactList from "./components/ContactList";
@@ -6,13 +6,22 @@ import Filter from "./components/Filter";
 
 import { v4 as uuidv4 } from "uuid";
 
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
+}
+
+
+
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ])
+  const [contacts, setContacts] = useLocalStorage('contacts', '')
 
   const [filter, setFilter] = useState('')
 
